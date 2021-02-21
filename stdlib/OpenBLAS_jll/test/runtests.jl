6b39b9334234b21779cfeae83c3bf84ee5f054f0
Viral B. Shah <viral@mayin.org>
2021-02-20 22:33:37 -0500

    Use 64-bit BLAS more consistently when Base.USE_BLAS64 is set

--- stdlib/OpenBLAS_jll/test/runtests.jl
+++ stdlib/OpenBLAS_jll/test/runtests.jl
@@ -1,8 +1,7 @@
 # This file is a part of Julia. License is MIT: https://julialang.org/license
 
-using Test, Libdl, OpenBLAS_jll
+using Test, Libdl, OpenBLAS_jll, LinearAlgebra
 
 @testset "OpenBLAS_jll" begin
-    @test dlsym(OpenBLAS_jll.libopenblas_handle, :openblas_set_num_threads; throw_error=false) !== nothing ||
-          dlsym(OpenBLAS_jll.libopenblas_handle, :openblas_set_num_threads64_; throw_error=false) !== nothing
+    @test dlsym(OpenBLAS_jll.libopenblas_handle, BLAS.@blasfunc(openblas_set_num_threads); throw_error=false) != nothing
 end
